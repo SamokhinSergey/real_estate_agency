787f40490a3b6cb67b4624cde7c58f8f8d4c7413
SERGEY SAMOKHIN <samokhin@live.ru>
2022-05-28 15:05:53 +0400

    add db model for complaints

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Flat
+from .models import Complaint
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -30,4 +31,12 @@ class FlatAdmin(admin.ModelAdmin):
     )
 
 
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = (
+        'user',
+        'flat',
+    )
+
+
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -53,6 +54,24 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
 
-
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    flat = models.ForeignKey(
+        Flat,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    text = models.TextField(
+        'Текст жалобы',
+        blank=True,
+        default=None,
+        null=True,
+    )
